f25c8e23a801d465384953994acf679d029c8cc1
aryanbindal86 <aryanbindal86@gmail.com>
2026-07-27 13:38:40 +0530

    Variable and Data type

---
+++ variables_and_Data_Types/Sum_three_numbers.java
@@ -0,0 +1,19 @@
+package variables_and_Data_Types;
+
+
+// Write a Program to sum three numbers in java??
+
+
+public class Sum_three_numbers {
+
+    public static void main(String[] args) {
+        System.out.println("The sum of these numbers is ");
+
+        int a = 22;
+        int b = 4;
+        int c = 9;
+        int sum = a + b + c ;
+        System.out.println(sum);
+
+    }
+}
