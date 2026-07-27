4a7ef0f4a278ac5b6f5d91f476d4fac2d063beaf
aryanbindal86 <aryanbindal86@gmail.com>
2026-07-27 17:22:04 +0530

    add file name lateral_in_java

---
+++ variables_and_Data_Types/literal_in_java.java
@@ -0,0 +1,29 @@
+package variables_and_Data_Types;
+
+public class literal_in_java{
+
+    public static void main(String[] args) {
+    // integer number
+      byte age = 34;
+      int age2 = 2;
+      short age3 = 44;
+      long dino = 1000000000000000000L;
+    //Character
+      char name = 'A';
+    //Decimal
+    //default value of decimal
+      float f1 = 3.4f;
+      double f2 = 3.44;
+
+
+    // Print Statement of all
+      System.out.println(age);
+      System.out.println(name);
+      System.out.println(f1);
+      System.out.println(f2);
+      System.out.println(dino);
+      System.out.println(age2);
+      System.out.println(age3);
+      
+    }
+}
