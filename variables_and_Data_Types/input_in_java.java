c0a3b68313ab91e06bfee99b134cacd4de012fd2
aryanbindal86 <aryanbindal86@gmail.com>
2026-07-28 08:17:49 +0530

    How to take input from the user

---
+++ variables_and_Data_Types/input_in_java.java
@@ -0,0 +1,22 @@
+package variables_and_Data_Types;
+import java.util.Scanner;
+
+
+public class input_in_java {
+
+    public static void main(String[] args) {
+        System.out.println("Taking Input Form the user");
+
+        Scanner sc = new Scanner(System.in);
+         
+         //System.out.println("Enter number 1:");
+         //int a = sc.nextInt();
+         //System.out.println("Enter number 2:");
+         //int b = sc.nextInt();
+         //int sum = a + b ;
+         //System.out.println("The sum of no is");
+         //System.out.println(sum);
+        boolean b = sc.hasNextInt();
+        System.out.println(b);           
+    }
+}
