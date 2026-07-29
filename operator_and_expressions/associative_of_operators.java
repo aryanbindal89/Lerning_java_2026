767bc690a31531a85769036d4e6ff8c2b188fc9f
aryanbindal86 <aryanbindal86@gmail.com>
2026-07-29 19:10:59 +0530

    associative of operators

---
+++ operator_and_expressions/associative_of_operators.java
@@ -0,0 +1,24 @@
+
+
+public class associative_of_operators{
+    public static void main(String[] args) {
+        //Precedence and associativity
+       // int a = 6*5-34/2;
+       // int b = 60/5-34*2;
+
+      //  System.out.println(a);
+        //System.out.println(b);
+        //Quick Quiz
+
+       // int x = 5; 
+        //int y =1;
+        //int k = x* y/2;
+
+        int a = 5;
+        int b = 1;
+        int c =4;
+        int k = b*b - (04*a*c)/(2*a);
+        System.out.println(k);
+    }
+
+}
