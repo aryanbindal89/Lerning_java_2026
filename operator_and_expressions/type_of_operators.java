b429d61be7c04ab568e026c0d41d1f223be9a5b1
aryanbindal86 <aryanbindal86@gmail.com>
2026-07-29 12:32:06 +0530

    Add type of operators

---
+++ operator_and_expressions/type_of_operators.java
@@ -0,0 +1,26 @@
+package operator_and_expressions;
+
+public class type_of_operators{
+    public static void main(String[] args) {
+        // a =99;
+       // int b = a +1 ;
+       // int b = a - 1 ;
+       //  int b = a % 4; //Modulo Operator help to find rememender.
+       int b = 9;
+       b += 3;
+       System.out.println(b);  
+
+       //Comparission Operator
+       System.out.println(8==9);
+       System.out.println(8!=9);
+       System.out.println(8>9);
+       System.out.println(8<9);
+
+        System.out.println(64>5 && 64>8);
+        System.out.println(64>5 && 64>98);
+      //  System.out.println(64>5||64>98);
+
+
+    }
+
+}
