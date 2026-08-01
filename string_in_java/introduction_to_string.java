4a5f8e6e7b912178166a77463f6d90ab2ac50ed0
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-01 08:04:24 +0530

    introduction to string

--- string_in_java/introduction_to_string.java
+++ string_in_java/introduction_to_string.java
@@ -2,6 +2,16 @@
 
 class introduction_to_string{
     public static void main(String[] args) {
-        
+        //String name = new String("Aryan");
+        System.out.print("My Name is: ");
+        String name = "Aryan";
+        System.out.println(name);
+
+
+        int a = 6;
+        float b = 5.66f;
+        System.out.printf("The value of a is %d and value of b is %f", a , b);
+        System.out.format("The value of a is %d and value of b is %f", a , b);
+
     }
 }
