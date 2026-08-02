88ef1da1b8a73f2c9a20f7efb4872970982ec6be
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-02 11:36:06 +0530

    if else statement

---
+++ conditiones_in_java/if_else_statement.java
@@ -0,0 +1,14 @@
+package conditiones_in_java;
+
+class if_else_statement{
+    public static void main(String[] args) {
+        int age = 18;
+        boolean cond = (age==18);
+        if(cond){
+            System.out.println("Yes boy you can Drive!!");
+        }
+        else{
+            System.out.println("You are not eligible for driving!!");
+        }
+    }
+}
