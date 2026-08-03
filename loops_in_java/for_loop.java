8a20056057d5d820b095221a43be64fba8a07ae7
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-03 17:49:59 +0530

    for loop

---
+++ loops_in_java/for_loop.java
@@ -0,0 +1,15 @@
+package loops_in_java;
+
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i = 1; i<=10; i++){
+            System.out.println(i);
+        }
+
+    // Quick Quiz = Write a program to print first n odd numbers 
+       int n = 5;
+        for(int j= 0; j<n; j++){
+            System.out.println(2*j+1);
+        }
+    }
+}
