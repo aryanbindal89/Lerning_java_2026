45f09972c8ca47b45aba2ac3669e03b185cffba7
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-03 07:57:18 +0530

    while loop

---
+++ loops_in_java/while_loop_in_java.java
@@ -0,0 +1,17 @@
+package loops_in_java;
+
+class while_loop_in_java{
+    public static void main(String[] args) {
+        System.out.println("Using While loop!!");
+        int i = 1;
+        while (i<=100) {
+            System.out.println(i);
+            i++;
+        }
+        int b = 101;
+        while (b<=200) {
+            System.out.println(b);
+            b++;
+        }
+    }
+}
