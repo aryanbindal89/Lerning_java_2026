6b67977c44bf4e27d765a8394f0087e2517b55ee
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-04 20:35:29 +0530

    practice set

---
+++ loops_in_java/practice_set_for_loop.java
@@ -0,0 +1,19 @@
+package loops_in_java;
+
+public class practice_set_for_loop {
+    public static void main(String[] args) {
+        //questin - 1 Write a java program to print the pattern 
+        //****
+        //***
+        //**
+        //*
+        int i =4;
+         
+        while (i>= 1){
+            System.out.println("*");
+            i++;
+            
+        }
+
+    }
+}
