338dd2454b2890ae5e11458f70f9be6be33e0fcc
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-05 09:14:11 +0530

    introduction to array

---
+++ Array_in_java/introduction_to_java.java
@@ -0,0 +1,19 @@
+package Array_in_java;
+
+public class introduction_to_java {
+    public static void main(String[] args) {
+        // int [] marks = new int[5];
+        int [] marks;
+        marks = new int[5];
+        marks [0] = 100;
+        marks [1] = 99;
+        marks [2] = 77;
+        marks [3] = 88;
+        marks [4] = 100;
+        System.out.println(marks[3]);
+
+        int [] a = {44, 44, 55 , 55 ,66};
+        System.out.println(a[0]);
+    }
+    
+}
