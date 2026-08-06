eb5abf7de083c3d0e4eec60dd12766f5861bcda3
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-06 21:17:53 +0530

    Lerninng metfuntional method

--- Methods_in_Java/method_overloading.java
+++ Methods_in_Java/method_overloading.java
@@ -8,19 +8,35 @@ public class method_overloading {
         arr[0]= 98;
     }
 
+
     static void telljoke(){
         System.out.println("I invited a new word!! \n   plagiarism!! ");
     }
+
+    static void foo(){
+        System.out.println("Good Morning Bro!!");
+    }
+    static void foo(int a){
+        System.out.println("Good Morning" + a + "bro!!");
+    }
     public static void main(String[] args) {
        // telljoke();
-       int [] marks = {55, 60 ,90 ,100 ,90};
-       change2(marks);
-       System.out.println(marks[0]);
+     //  int [] marks = {55, 60 ,90 ,100 ,90};
+      // change2(marks);
+       //System.out.println(marks[0]);
 
        //Changinng the innteger
        /*  int x = 45;
         change(x);
         System.out.println("The value of x after changing is " + x);
         */
+
+
+        //Method Overloading
+        foo();
+        int a = 100;
+        foo(a);
+        //Arguments are actual!!
+        
     }
 }
