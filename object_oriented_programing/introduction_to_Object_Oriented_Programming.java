93919c21f1104e29765ccf0eab5825a276c023d4
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-08 20:16:56 +0530

    just edit

--- object_oriented_programing/introduction_to_Object_Oriented_Programming.java
+++ object_oriented_programing/introduction_to_Object_Oriented_Programming.java
@@ -2,6 +2,9 @@ package object_oriented_programing;
 
 public class introduction_to_Object_Oriented_Programming {
     public static void main(String[] args) {
-        
+        System.out.println("For tommarow");
+        System.out.println("kal k liye");
+        String a = "Sab kal hoga light nhi a rhi ";
+        System.out.println(a);
     }
 }
