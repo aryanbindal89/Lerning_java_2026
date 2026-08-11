97d99ab0a5504b08a141d97133952c26343e7a85
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-11 13:11:34 +0530

    getter and setter

--- access_modifiers_and_constructors/access_modifiers_getters_and_setters_in_Java.java
+++ access_modifiers_and_constructors/access_modifiers_getters_and_setters_in_Java.java
@@ -1,5 +1,28 @@
 package access_modifiers_and_constructors;
 
 public class access_modifiers_getters_and_setters_in_Java {
-    
+   static class myEmploy{
+        private int id;
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String n){
+            name = n;
+        }
+        public void setID(int i){
+            id = i;
+        }
+        public int getID(){
+            return id;
+        }
+    }
+    public static void main(String[] args) {
+        myEmploy aryan = new myEmploy();
+        aryan.id = 1;
+        aryan.name = "Aryan Agrawal";
+
+    }
 }
