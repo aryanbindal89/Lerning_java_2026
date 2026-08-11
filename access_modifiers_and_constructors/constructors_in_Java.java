732970cd481f6152c583946690bfccd12893608b
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-11 20:47:53 +0530

    edit the file

--- access_modifiers_and_constructors/constructors_in_Java.java
+++ access_modifiers_and_constructors/constructors_in_Java.java
@@ -1,5 +1,26 @@
 package access_modifiers_and_constructors;
 
 public class constructors_in_Java {
-    
+     static class myEmploy{
+        private int id;
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String n){
+            name = n;
+        }
+        public void setID(int i){
+            id = i;
+        }
+        public int getID(){
+            return id;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
