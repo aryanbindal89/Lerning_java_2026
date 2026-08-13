9a61916a06b22b9215cc20f421c07c4264cca9bd
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-13 21:16:32 +0530

    complted basic of inheritence

--- interitence_in_java/inheritence_in_java.java
+++ interitence_in_java/inheritence_in_java.java
@@ -1,14 +1,52 @@
 package interitence_in_java;
 
 public class inheritence_in_java {
-    class base{
-        int x;
-        base(){
 
+    class base {
+        int x;
+
+        public int getx() {
+            return x;
+        }
+
+        public void setx(int x) {
+            this.x = x;
+        }
+
+        public void Printme() {
+            System.out.println("I am a Constructor");
         }
     }
+
+    class Derived extends base {
+        int y;
+
+        public int gety() {
+            return y;
+        }
+
+        public void sety(int y) {
+            this.y = y;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("just edit file ");
-        
+
+        inheritence_in_java obj = new inheritence_in_java();
+
+        // Object of base class
+        base b = obj.new base();
+
+        b.setx(10);
+        System.out.println(b.getx());
+
+        // Object of Derived class
+        Derived a = obj.new Derived();
+
+        a.setx(20);          // inherited from base
+        System.out.println(a.getx());
+
+        a.sety(30);          // Derived's own method
+        System.out.println(a.gety());
     }
 }
