306a2f086dc57faf1ee305bdc1a751106e95acbf
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-13 19:21:18 +0530

    edit inheritence in java file

--- interitence_in_java/inheritence_in_java.java
+++ interitence_in_java/inheritence_in_java.java
@@ -1,7 +1,14 @@
 package interitence_in_java;
 
 public class inheritence_in_java {
+    class base{
+        int x;
+        base(){
+
+        }
+    }
     public static void main(String[] args) {
+        System.out.println("just edit file ");
         
     }
 }
