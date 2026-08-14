1a1fc7fd1c5a0e88389098acc5909e03e37db983
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-14 20:59:11 +0530

    completed constructor in inheritence

--- interitence_in_java/constructor_in_inheritence.java
+++ interitence_in_java/constructor_in_inheritence.java
@@ -1,21 +1,27 @@
 package interitence_in_java;
 
 public class constructor_in_inheritence {
-    class base{
-        public int x;
-        public int getx(){
-            return x;
+    class base1{
+        base1(){
+            System.out.println("I am a constructor");
         }
-        public void setx(int x){
-            this.x =x ;
+                base1(int a){
+            System.out.println("I am a overloaded constructor with value of a as:" + a);
         }
     }
+    
 
-    class derived extends base{
-        public int y;
+    class derived1 extends base1{
+        derived1(){
+            super(0);
+            System.out.println("I am contructor of derived class...");
+        }
     }
 
     public static void main(String[] args) {
-        
+        constructor_in_inheritence obj = new constructor_in_inheritence();
+        base1 b = obj.new base1();
+        derived1 d = obj.new derived1();
+
     }
 }
