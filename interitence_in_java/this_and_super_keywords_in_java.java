2d3a5b1cb3e9c78ac943dfeecdf6b6b4b92d5402
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-14 22:24:55 +0530

    This and super keyword

--- interitence_in_java/this_and_super_keywords_in_java.java
+++ interitence_in_java/this_and_super_keywords_in_java.java
@@ -1,7 +1,33 @@
 package interitence_in_java;
 
+import interitence_in_java.this_and_super_keywords_in_java.ekclass;
+
 public class this_and_super_keywords_in_java {
+    class ekclass{
+        int a;
+        public int getA(){
+            return a;
+        }
+        ekclass(int v){
+           this.a = v;
+        }
+        public int returnone(){
+            return 1;
+        }
+    }
+
+    class doclass extends ekclass{
+        doclass(int c){
+            super(c);
+            System.out.println("I am a constructor...");
+        }
+    }
     public static void main(String[] args) {
-        
+        this_and_super_keywords_in_java obj = new this_and_super_keywords_in_java();
+        ekclass e = obj.new ekclass(65); 
+        doclass d = obj.new doclass(33);
+        System.out.println(e.getA());
+
+
     }
 }
