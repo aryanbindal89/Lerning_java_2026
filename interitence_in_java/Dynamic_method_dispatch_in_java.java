70021df15443ecfb221b4a5b4a2c87ad32d548e9
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-15 21:35:48 +0530

    edit file

--- interitence_in_java/Dynamic_method_dispatch_in_java.java
+++ interitence_in_java/Dynamic_method_dispatch_in_java.java
@@ -1,7 +1,34 @@
 package interitence_in_java;
 
+import operator_and_expressions.type_of_operators;
+
+class one{
+        public void great(){
+        System.out.println("Good Morning");
+    }
+    public void name(){
+        System.out.println("My name is Aryan Agrawal");
+    }
+}
+
+
+    class two extends one{
+        public void swagat(){
+            System.out.println("Apka swagat h!!");
+        }
+         public void name(){
+        System.out.println("My name is Aryan Agrawal of class 2");
+    }
+    }
+
+
 public class Dynamic_method_dispatch_in_java {
     public static void main(String[] args) {
-        
+       // one obj = new one();
+       // two obj2 = new two();
+       // obj.name();
+       one obj = new two();
+       obj.great();
+       obj.name();
     }
 }
