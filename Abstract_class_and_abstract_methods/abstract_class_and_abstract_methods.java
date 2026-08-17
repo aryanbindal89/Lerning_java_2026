203f60db309c5d8fc6e5d81a810952b7aa69f06f
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-17 13:48:44 +0530

    completed abstract class

--- Abstract_class_and_abstract_methods/abstract_class_and_abstract_methods.java
+++ Abstract_class_and_abstract_methods/abstract_class_and_abstract_methods.java
@@ -1,7 +1,41 @@
 package Abstract_class_and_abstract_methods;
 
 public class abstract_class_and_abstract_methods {
+
+    static abstract class Parent {
+        public Parent() {
+            System.out.println("I am constructor of base...");
+        }
+
+        public void sayhello() {
+            System.out.println("Hello...");
+        }
+
+        abstract public void greet();
+    }
+
+    static class Child extends Parent {
+        @Override
+        public void greet() {
+            System.out.println("Good Morning!!");
+        }
+    }
+
+    static abstract class Child2 extends Child {
+        public void th() {
+            System.out.println("I am Good...");
+        }
+    }
+
     public static void main(String[] args) {
-        
+
+        Parent p = new Child();
+        Child c = new Child();
+
+        p.greet();
+        p.sayhello();
+
+        c.greet();
+        c.sayhello();
     }
 }
