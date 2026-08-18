238da4989175499d225386d11d6eaad42b8ad511
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-18 20:10:06 +0530

    make a file name abstract class VS interface

---
+++ Abstract_class_and_abstract_methods/abstract_classes_vs_interfaces.java
@@ -0,0 +1,12 @@
+package Abstract_class_and_abstract_methods;
+
+interface Bicycle{
+    void applyBreak(int decrement);
+    void speedUp(int increment);
+}
+
+public class abstract_classes_vs_interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
