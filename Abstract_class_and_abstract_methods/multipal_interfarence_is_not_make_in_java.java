c32ffb6ad651fff09316bf66742082bfc573d605
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-19 22:10:17 +0530

    just theory based question

--- Abstract_class_and_abstract_methods/multipal_interfarence_is_not_make_in_java.java
+++ Abstract_class_and_abstract_methods/multipal_interfarence_is_not_make_in_java.java
@@ -1,5 +1,7 @@
 package Abstract_class_and_abstract_methods;
 
 public class multipal_interfarence_is_not_make_in_java {
-    
+    public static void main(String[] args) {
+        System.out.println("just theory based question...");
+    }
 }
