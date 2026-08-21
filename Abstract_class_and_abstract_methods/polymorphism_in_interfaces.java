e94ed6accf365481dadc75a76fcf6b2a7b009750
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-21 20:17:41 +0530

    just learned Polymorphism in Interfaces

--- Abstract_class_and_abstract_methods/polymorphism_in_interfaces.java
+++ Abstract_class_and_abstract_methods/polymorphism_in_interfaces.java
@@ -2,6 +2,6 @@ package Abstract_class_and_abstract_methods;
 
 public class polymorphism_in_interfaces {
     public static void main(String[] args) {
-        
+        System.out.println("Just Learned it...");
     }
 }
