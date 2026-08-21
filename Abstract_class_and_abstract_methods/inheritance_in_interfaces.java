2ce1c6edd47c218fda1168af7deb3e0e5f952f00
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-21 19:18:55 +0530

    completed

--- Abstract_class_and_abstract_methods/inheritance_in_interfaces.java
+++ Abstract_class_and_abstract_methods/inheritance_in_interfaces.java
@@ -1,7 +1,36 @@
 package Abstract_class_and_abstract_methods;
 
+interface sampleInterfeace{
+    void meth1();
+    void meth2();
+}
+
+interface childSampleInterface extends sampleInterfeace{
+    void meth3();
+    void meth4();
+}
+
+class MySampleClass implements childSampleInterface{
+    public void meth1(){
+        System.out.println("Meth1");
+    }
+    public void meth2(){
+        System.out.println("Meth2");
+    }
+    public void meth3(){
+        System.out.println("Meth3");
+    }
+    public void meth4(){
+        System.out.println("Meth4");
+    }
+}
+
 public class inheritance_in_interfaces {
     public static void main(String[] args) {
-        
+        MySampleClass obj = new MySampleClass();
+        obj.meth1();
+        obj.meth2();
+        obj.meth3();
+        obj.meth4();
     }
 }
