1cd6fcb4129c334ea49d60a7f5b70b38eae9538e
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-22 16:09:41 +0530

    completed

--- Packages_in_Java/creating_packages_in_java.java
+++ Packages_in_Java/creating_packages_in_java.java
@@ -1,5 +1,13 @@
 package Packages_in_Java;
 
+import java.util.Scanner;
+
 public class creating_packages_in_java {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+    System.out.println("This is my Scanner taking input as " + a);
+    sc.close();        
+    }
     
 }
