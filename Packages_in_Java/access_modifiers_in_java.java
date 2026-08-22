66fdfb3d3f13fb17bff97f2ad345bad3bb55a926
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-22 16:16:22 +0530

    Completed

--- Packages_in_Java/access_modifiers_in_java.java
+++ Packages_in_Java/access_modifiers_in_java.java
@@ -2,6 +2,6 @@ package Packages_in_Java;
 
 public class access_modifiers_in_java {
     public static void main(String[] args) {
-        
+        System.out.println("just learned it...");
     }
 }
