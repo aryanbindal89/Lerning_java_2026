1172bb50bd87b72a3c330667a04bf7afa79c464c
aryanbindal86 <aryanbindal86@gmail.com>
2026-08-22 22:05:28 +0530

    just a understanding chapter

--- Multi_thereading/multithreading_in_java.java
+++ Multi_thereading/multithreading_in_java.java
@@ -2,6 +2,6 @@ package Multi_thereading;
 
 public class multithreading_in_java {
     public static void main(String[] args) {
-        
+        System.out.println("Just a understanding Chapter...");
     }
 }
